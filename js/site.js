d5b820d1cde58085f214351179da9931d0abd234
Tanner <tannerroymartin@gmail.com>
2021-04-19 10:08:41 -0400

    Changed table background, added prism

--- js/site.js
+++ js/site.js
@@ -1,12 +1,4 @@
-//FORM for User input
-// let loanAmount = parseInt(document.getElementById("loanAmount").value);
-// let term = parseInt(document.getElementById("term").value);
-// let interestRate = parseInt(document.getElementById("interestRate").value);
-// let totalCost = ((document.getElementById("loanAmount")) * (document.getElementById("term")) * (document.getElementById("interestRate")));
-// let monthlyPaymentSummary = (loanAmount) * (interestRate / 1200) / (1 - Math.pow((1 + interestRate / 1200), (-term)));
-
 loanArray = [];
-
 
 function calculate() {
     //User inputs
@@ -25,7 +17,6 @@ function calculate() {
     let interest = 0;
     let totalInterest = 0;
     let remainingBalance = 0;
-    let rate = 0;
     let principalPayment = 0;
     let monthlyPayment = (principal) * (interestRate / 1200) / (1 - Math.pow((1 + interestRate / 1200), (-months)));
     let interestPayment = 0;
